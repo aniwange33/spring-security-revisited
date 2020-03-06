5fbeb4dd1b7c80039e91444b229ed4e4b8f2a60d
aniwange <terteseamos@gmail.com>
2020-03-06 11:03:09 +0100

    feat: logout password added

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -54,7 +55,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .rememberMe() // default for two weeks
                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                .key("somethingspecial");
+                .key("somethingspecial")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me" )
+                    .logoutSuccessUrl("/login");
     }
 
     @Bean
